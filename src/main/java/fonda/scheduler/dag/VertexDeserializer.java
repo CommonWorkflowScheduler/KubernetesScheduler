495675252baccff9012fe651f0d05f7697867a82
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2022-02-10 12:40:17 +0100

    Also parse NodeVertices
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/fonda/scheduler/dag/VertexDeserializer.java
+++ src/main/java/fonda/scheduler/dag/VertexDeserializer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.NullNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 import org.springframework.boot.jackson.JsonComponent;
 
@@ -19,7 +20,8 @@ public class VertexDeserializer extends JsonDeserializer<Vertex> {
         final TreeNode treeNode = jsonParser.getCodec().readTree(jsonParser);
 
         final Type type = Type.valueOf(((TextNode) treeNode.get("type")).asText());
-        final String label = ((TextNode) treeNode.get("label")).asText();
+        final TreeNode labelNode = treeNode.get("label");
+        final String label = labelNode instanceof NullNode ? null : ((TextNode) labelNode).asText();
         final int uid =  ((IntNode) treeNode.get("uid")).asInt();
 
         if ( Type.PROCESS == type ) {
@@ -28,8 +30,10 @@ public class VertexDeserializer extends JsonDeserializer<Vertex> {
             return new Operator( label, uid );
         } else if ( Type.ORIGIN == type ) {
             return new Origin( label, uid );
+        }  else if ( Type.NODE == type ) {
+            return new Node( label, uid );
         } else {
-            throw new IllegalArgumentException( "No implementation for type: " + type );
+            throw new IllegalArgumentException( "No implementation for type: " + type + "(" + treeNode + ")" );
         }
 
     }
