b53750db30264faf769e812df06cbf70ff6c1432
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2022-02-10 11:26:05 +0100

    Parse also dates with format MMM dd HH:mm:ss yyyy
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/fonda/scheduler/model/TaskResultParser.java
+++ src/main/java/fonda/scheduler/model/TaskResultParser.java
@@ -115,17 +115,23 @@ public class TaskResultParser {
 
     private long fileTimeFromString(String date) {
         if( date == null || date.equals("-")) {
-            return -1;
+            throw new RuntimeException("Date was: " + date);
         }
-        String[] parts = date.split(" ");
-        parts[1] = parts[1].substring(0, 12);
-        String shortenedDate = String.join(" ", parts);
         try {
-            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS Z").parse(shortenedDate).getTime();
-        } catch (ParseException e) {
-            e.printStackTrace();
+            if (Character.isLetter(date.charAt(0))) {
+                // if ls was used, date has the format "Nov 2 08:49:30 2021"
+                return new SimpleDateFormat("MMM dd HH:mm:ss yyyy").parse(date).getTime();
+            } else {
+                // if stat was used, date has the format "2021-11-02 08:49:30.955691861 +0000"
+                String[] parts = date.split(" ");
+                parts[1] = parts[1].substring(0, 12);
+                // parts[1] now has milliseconds as smallest units e.g. "08:49:30.955"
+                String shortenedDate = String.join(" ", parts);
+                return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS Z").parse(shortenedDate).getTime();
+            }
+        } catch ( Exception e ){
+            throw new RuntimeException( "Cannot parse date: " + date, e );
         }
-        return -1;
     }
 
 }
