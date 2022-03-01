42295b6dd69437c29c53016e35e61794b1762210
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2022-03-01 10:22:59 +0100

    Date parser in own class
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/fonda/scheduler/model/TaskResultParser.java
+++ src/main/java/fonda/scheduler/model/TaskResultParser.java
@@ -95,7 +95,7 @@ public class TaskResultParser {
                     {
                             final LocationWrapper locationWrapper = new LocationWrapper(
                                                                             location,
-                                                                            fileTimeFromString(modificationDate),
+                                                                            DateParser.millisFromString(modificationDate),
                                                                             Long.parseLong(data[ SIZE ]),
                                                                             finishedTask
                                                                     );
@@ -111,27 +111,6 @@ public class TaskResultParser {
         }
         return newOrUpdated;
 
-    }
-
-    private long fileTimeFromString(String date) {
-        if( date == null || date.equals("-")) {
-            throw new RuntimeException("Date was: " + date);
-        }
-        try {
-            if (Character.isLetter(date.charAt(0))) {
-                // if ls was used, date has the format "Nov 2 08:49:30 2021"
-                return new SimpleDateFormat("MMM dd HH:mm:ss yyyy").parse(date).getTime();
-            } else {
-                // if stat was used, date has the format "2021-11-02 08:49:30.955691861 +0000"
-                String[] parts = date.split(" ");
-                parts[1] = parts[1].substring(0, 12);
-                // parts[1] now has milliseconds as smallest units e.g. "08:49:30.955"
-                String shortenedDate = String.join(" ", parts);
-                return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS Z").parse(shortenedDate).getTime();
-            }
-        } catch ( Exception e ){
-            throw new RuntimeException( "Cannot parse date: " + date, e );
-        }
     }
 
 }
