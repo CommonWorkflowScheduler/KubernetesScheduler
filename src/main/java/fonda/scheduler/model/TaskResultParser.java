6e80d9ddd5f801ecafcad4e0d2e1eb624270d2c2
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2022-03-01 11:36:53 +0100

    Remove unused import
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/fonda/scheduler/model/TaskResultParser.java
+++ src/main/java/fonda/scheduler/model/TaskResultParser.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Stream;
 
