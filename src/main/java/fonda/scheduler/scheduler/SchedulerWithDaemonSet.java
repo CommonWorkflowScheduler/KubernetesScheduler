e5fcc157bc94dbc40f6d80df0db953bec2ad03f3
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2022-03-25 12:51:07 +0100

    Collect Input Files: moved logic into separate class.
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/fonda/scheduler/scheduler/SchedulerWithDaemonSet.java
+++ src/main/java/fonda/scheduler/scheduler/SchedulerWithDaemonSet.java
@@ -47,11 +47,13 @@ public abstract class SchedulerWithDaemonSet extends Scheduler {
     @Getter
     private final CopyStrategy copyStrategy;
     final HierarchyWrapper hierarchyWrapper;
+    private final InputFileCollector inputFileCollector;
     private final ConcurrentHashMap<Long,LocationWrapper> requestedLocations = new ConcurrentHashMap<>();
 
     SchedulerWithDaemonSet(String execution, KubernetesClient client, String namespace, SchedulerConfig config) {
         super(execution, client, namespace, config);
         this.hierarchyWrapper = new HierarchyWrapper( config.workDir );
+        this.inputFileCollector = new InputFileCollector( hierarchyWrapper );
         if ( config.copyStrategy == null ) throw new IllegalArgumentException( "Copy strategy is null" );
         switch ( config.copyStrategy ){
             case "ftp":
@@ -164,62 +166,10 @@ public abstract class SchedulerWithDaemonSet extends Scheduler {
         return null;
     }
 
-    private LinkedList<Tuple<HierarchyFile,Path>> filterFilesToProcess(List<InputParam<FileHolder>> fileInputs ){
-        final LinkedList<Tuple<HierarchyFile,Path>> toProcess = new LinkedList<>();
-        for ( InputParam<FileHolder> fileInput : fileInputs) {
-            final Path path = Path.of(fileInput.value.sourceObj);
-            if ( this.hierarchyWrapper.isInScope( path ) ){
-                toProcess.add( new Tuple<>(hierarchyWrapper.getFile( path ), path) );
-            }
-        }
-        return toProcess;
-    }
-    private void processNext(
-            final LinkedList<Tuple<HierarchyFile,Path>> toProcess,
-            final List<SymlinkInput> symlinks,
-            final List<PathFileLocationTriple> files,
-            final Set<Location> excludedLocations,
-            final Task task
-    ){
-        final Tuple<HierarchyFile, Path> tuple = toProcess.removeLast();
-        final HierarchyFile file = tuple.getA();
-        if( file == null ) return;
-        final Path path = tuple.getB();
-        if ( file.isSymlink() ){
-            final Path linkTo = ((LinkHierarchyFile) file).getDst();
-            symlinks.add( new SymlinkInput( path, linkTo ) );
-            toProcess.push( new Tuple<>( hierarchyWrapper.getFile( linkTo ), linkTo ) );
-        } else if( file.isDirectory() ){
-            final Map<Path, AbstractHierarchyFile> allChildren = ((Folder) file).getAllChildren(path);
-            for (Map.Entry<Path, AbstractHierarchyFile> pathAbstractFileEntry : allChildren.entrySet()) {
-                toProcess.push( new Tuple<>( pathAbstractFileEntry.getValue(), pathAbstractFileEntry.getKey() ) );
-            }
-        } else {
-            final RealHierarchyFile realFile = (RealHierarchyFile) file;
-            final RealHierarchyFile.MatchingLocationsPair filesForTask = realFile.getFilesForTask(task);
-            if ( filesForTask.getExcludedNodes() != null ) {
-                excludedLocations.addAll(filesForTask.getExcludedNodes());
-            }
-            files.add( new PathFileLocationTriple( path, realFile, filesForTask.getMatchingLocations()) );
-        }
+    TaskInputs getInputsOfTask(Task task ){
+        return inputFileCollector.getInputsOfTask( task );
     }
 
-    TaskInputs getInputsOfTask( Task task ){
-
-        final List<InputParam<FileHolder>> fileInputs = task.getConfig().getInputs().fileInputs;
-        final LinkedList<Tuple<HierarchyFile,Path>> toProcess = filterFilesToProcess( fileInputs );
-
-        final List<SymlinkInput> symlinks = new ArrayList<>( fileInputs.size() );
-        final List<PathFileLocationTriple> files = new ArrayList<>( fileInputs.size() );
-        final Set<Location> excludedLocations = new HashSet<>();
-
-        while ( !toProcess.isEmpty() ){
-            processNext( toProcess, symlinks, files, excludedLocations, task );
-        }
-
-        return new TaskInputs( symlinks, files, excludedLocations );
-
-    }
 
     public FileResponse nodeOfLastFileVersion( String path ) throws NotARealFileException {
         LinkedList<SymlinkInput> symlinks = new LinkedList<>();
