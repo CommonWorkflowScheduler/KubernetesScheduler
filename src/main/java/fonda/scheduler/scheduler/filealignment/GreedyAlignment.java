b9eff4b1dc97c201bc15200007892d0e1bc594d0
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2022-05-06 10:24:37 +0200

    Refactored code
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/fonda/scheduler/scheduler/filealignment/GreedyAlignment.java
+++ src/main/java/fonda/scheduler/scheduler/filealignment/GreedyAlignment.java
@@ -33,14 +33,13 @@ public class GreedyAlignment implements InputAlignment {
             LocationWrapper bestLocationWrapper = null;
             for (LocationWrapper locationWrapper : pathFileLocationTriple.locations) {
                 final Location currentLoc = locationWrapper.getLocation();
+                final double calculatedCost;
                 if ( currentLoc != node.getNodeLocation() ) {
-                    minCost = 0;
-                    bestLoc = currentLoc;
-                    bestLocationWrapper = locationWrapper;
-                    break;
+                    final AlignmentWrapper alignmentWrapper = map.get(currentLoc);
+                    calculatedCost = cf.calculateCost(alignmentWrapper, locationWrapper);
+                } else {
+                    calculatedCost = 0;
                 }
-                final AlignmentWrapper alignmentWrapper = map.get(currentLoc);
-                final double calculatedCost = cf.calculateCost(alignmentWrapper, locationWrapper);
                 if ( calculatedCost < minCost ) {
                     bestLoc = currentLoc;
                     minCost = calculatedCost;
