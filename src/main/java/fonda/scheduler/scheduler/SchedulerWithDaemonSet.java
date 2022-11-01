d1e73510a53b334847f73ff134251476c8fd8591
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2022-11-01 14:03:01 +0100

    changed visibility
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/fonda/scheduler/scheduler/SchedulerWithDaemonSet.java
+++ src/main/java/fonda/scheduler/scheduler/SchedulerWithDaemonSet.java
@@ -456,7 +456,7 @@ public abstract class SchedulerWithDaemonSet extends Scheduler {
     }
 
     @Override
-    boolean canSchedulePodOnNode(Requirements availableByNode, PodWithAge pod, NodeWithAlloc node ) {
+    public boolean canSchedulePodOnNode( Requirements availableByNode, PodWithAge pod, NodeWithAlloc node ) {
         return this.getDaemonOnNode( node ) != null && super.canSchedulePodOnNode( availableByNode, pod, node );
     }
 
