96903c84bf90da3a21d1c6cb9cdceb8676d5915b
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2022-12-23 12:39:42 +0100

    First version of separate copy tasks (LAv2)
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/fonda/scheduler/model/taskinputs/TaskInputs.java
+++ src/main/java/fonda/scheduler/model/taskinputs/TaskInputs.java
@@ -1,14 +1,19 @@
 package fonda.scheduler.model.taskinputs;
 
 import fonda.scheduler.model.location.Location;
+import fonda.scheduler.model.location.hierachy.LocationWrapper;
+import fonda.scheduler.util.Tuple;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @ToString
 @Getter
+@Slf4j
 public class TaskInputs {
 
     private final List<SymlinkInput> symlinks;
@@ -23,14 +28,42 @@ public class TaskInputs {
         this.excludedNodes = excludedNodes;
     }
 
+    public boolean allFilesAreOnLocationAndNotOverwritten( Location loc, Set<String> pathCurrentlyCopying ){
+        for (PathFileLocationTriple file : files) {
+            if ( !file.locatedOnLocation(loc) || (pathCurrentlyCopying != null && pathCurrentlyCopying.contains(file.path.toString())) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<LocationWrapper> allLocationWrapperOnLocation( Location loc){
+        return files
+                .parallelStream()
+                .map( file -> file.locationWrapperOnLocation( loc ) )
+                .collect( Collectors.toList() );
+    }
+
     public long calculateDataOnNode( Location loc ) {
+        return calculateDataOnNodeAdditionalInfo( loc ).getB();
+    }
+
+    /**
+     * Calculates the data on a node and returns whether all data is on the location
+     * @param loc
+     * @return boolean: true if all files are on location, Long: data on location
+     */
+    public Tuple<Boolean,Long> calculateDataOnNodeAdditionalInfo( Location loc ) {
         long size = 0;
+        boolean allOnNode = true;
         for ( PathFileLocationTriple fileLocation : files ) {
             if (fileLocation.locatedOnLocation(loc)) {
                 size += fileLocation.getSizeInBytes();
+            } else {
+                allOnNode = false;
             }
         }
-        return size;
+        return new Tuple<>( allOnNode, size );
     }
 
     public long calculateAvgSize() {
