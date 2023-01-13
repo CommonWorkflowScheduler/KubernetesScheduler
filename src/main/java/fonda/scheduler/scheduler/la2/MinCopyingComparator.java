e0cbbc8f1a1155e5861b8d8860c03a2cba2c5400
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2023-01-13 14:09:38 +0100

    Compare whether the task is already copied somewhere.
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/fonda/scheduler/scheduler/la2/MinCopyingComparator.java
+++ src/main/java/fonda/scheduler/scheduler/la2/MinCopyingComparator.java
@@ -14,6 +14,10 @@ public class MinCopyingComparator extends TaskStatComparator {
             return -1;
         } else if ( o1.getCompleteOnNodes() > o2.getCompleteOnNodes() ) {
             return 1;
+        } else if ( o1.getCopyingToNodes() < o2.getCopyingToNodes() ) {
+            return -1;
+        } else if ( o1.getCopyingToNodes() > o2.getCopyingToNodes() ) {
+            return 1;
         } else {
             return getComparator().compare( o1.getBestStats(), o2.getBestStats() );
         }
