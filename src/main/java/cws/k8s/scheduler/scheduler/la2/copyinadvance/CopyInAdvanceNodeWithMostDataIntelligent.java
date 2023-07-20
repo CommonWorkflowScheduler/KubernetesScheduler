a7cf6f36eb88f38f877a05f45ff13c012112b515
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2023-07-20 16:33:32 +0200

    First version of a more intelligent strategy for phase 3
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/cws/k8s/scheduler/scheduler/la2/copyinadvance/CopyInAdvanceNodeWithMostDataIntelligent.java
+++ src/main/java/cws/k8s/scheduler/scheduler/la2/copyinadvance/CopyInAdvanceNodeWithMostDataIntelligent.java
@@ -1,10 +1,10 @@
 package cws.k8s.scheduler.scheduler.la2.copyinadvance;
 
 import cws.k8s.scheduler.model.NodeWithAlloc;
+import cws.k8s.scheduler.model.Requirements;
 import cws.k8s.scheduler.model.Task;
 import cws.k8s.scheduler.model.location.NodeLocation;
 import cws.k8s.scheduler.scheduler.filealignment.InputAlignment;
-import cws.k8s.scheduler.scheduler.la2.CreateCopyTasks;
 import cws.k8s.scheduler.scheduler.la2.TaskStat;
 import cws.k8s.scheduler.util.NodeTaskFilesAlignment;
 import cws.k8s.scheduler.util.SortedList;
@@ -12,9 +12,8 @@ import cws.k8s.scheduler.util.TaskStats;
 import cws.k8s.scheduler.util.copying.CurrentlyCopying;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class CopyInAdvanceNodeWithMostDataIntelligent extends CopyInAdvance {
@@ -22,7 +21,8 @@ public class CopyInAdvanceNodeWithMostDataIntelligent extends CopyInAdvance {
     /**
      * Try to distribute the tasks evenly on the nodes. More important tasks can be on x more nodes. (x = TASKS_READY_FACTOR)
      */
-    private final int TASKS_READY_FACTOR = 1;
+    private static final int TASKS_READY_FACTOR = 2;
+    private static final RequirementsComparator requirementsComparator = new RequirementsComparator();
 
     public CopyInAdvanceNodeWithMostDataIntelligent(
             CurrentlyCopying currentlyCopying,
@@ -43,8 +43,11 @@ public class CopyInAdvanceNodeWithMostDataIntelligent extends CopyInAdvance {
             final int maxCopyingTaskPerNode,
             final int maxHeldCopyTaskReady,
             final Map<NodeLocation, Integer> currentlyCopyingTasksOnNode,
-            int prio
+            int prio,
+            Map<NodeWithAlloc, List<Task>> readyTasksPerNode
     ) {
+        Map<NodeWithAlloc,NodeCache> cache = new HashMap<>();
+        long startMethod = System.currentTimeMillis();
         final SortedList<TaskStat> stats = new SortedList<>( taskStats.getTaskStats() );
         removeTasksThatAreCopiedMoreThanXTimeCurrently( stats, copySameTaskInParallel );
 
@@ -54,8 +57,9 @@ public class CopyInAdvanceNodeWithMostDataIntelligent extends CopyInAdvance {
             LinkedList<TaskStat> tasksReadyOnMoreNodes = new LinkedList<>();
             while( !stats.isEmpty() ) {
                 final TaskStat poll = stats.poll();
+                long start = System.currentTimeMillis();
 
-                if ( poll.dataOnNodes() + TASKS_READY_FACTOR > readyOnNodes ) {
+                if ( poll.dataOnNodes() > readyOnNodes + TASKS_READY_FACTOR ) {
                     tasksReadyOnMoreNodes.add( poll );
                     continue;
                 }
@@ -63,30 +67,103 @@ public class CopyInAdvanceNodeWithMostDataIntelligent extends CopyInAdvance {
                 final TaskStat.NodeAndStatWrapper bestStats = poll.getBestStats();
                 final Task task = poll.getTask();
                 final NodeWithAlloc node = bestStats.getNode();
-
-                final boolean cannotAdd;
+                final NodeCache nodeCache = cache.computeIfAbsent( node,
+                        n -> new NodeCache(
+                                //Only consider workflow tasks, other will not finish soon
+                                n.getAssignedPods().entrySet().stream().filter( x -> x.getKey().contains( "||nf-" ) ).map( x -> x.getValue() ).collect( Collectors.toList() ),
+                                readyTasksPerNode.get( n ),
+                                planedToCopy.getTasksOnNode( n.getNodeLocation() )
+                        )
+                );
 
                 //Check if the node has still enough resources to run the task
-                if ( currentlyCopyingTasksOnNode.getOrDefault( node.getNodeLocation(), 0 ) < maxCopyingTaskPerNode ) {
-                    if ( createFileAlignment( planedToCopy, nodeTaskFilesAlignments, currentlyCopyingTasksOnNode, poll, task, node, prio ) ) {
-                        cannotAdd = false;
+                if ( currentlyCopyingTasksOnNode.getOrDefault( node.getNodeLocation(), 0 ) < maxCopyingTaskPerNode
+                    && reasonableToCopyData( node, task, nodeCache )
+                    && createFileAlignment( planedToCopy, nodeTaskFilesAlignments, currentlyCopyingTasksOnNode, poll, task, node, prio ) )
+                {
                         log.info( "Start copy task with {} missing bytes", poll.getBestStats().getTaskNodeStats().getSizeRemaining() );
-                    } else {
-                        cannotAdd = true;
-                    }
+                        nodeCache.addPlaned( task.getRequest() );
                 } else {
-                    cannotAdd = true;
+                    //if not enough resources or too many tasks are running, mark next node as to compare and add again into the list
+                    if ( poll.increaseIndexToCompare() ) {
+                        //Only re-add if still other opportunities exist
+                        stats.add( poll );
+                    }
                 }
-                //if not enough resources or too many tasks are running, mark next node as to compare and add again into the list
-                if ( cannotAdd && poll.increaseIndexToCompare() ) {
-                    //Only re-add if still other opportunities exist
-                    stats.add( poll );
-                }
+                task.getTraceRecord().addSchedulerTimeDeltaPhaseThree( (int) (System.currentTimeMillis() - start) );
             }
             stats.addAll( tasksReadyOnMoreNodes );
             readyOnNodes++;
         }
+        log.info( "Time to create alignment for tasks with enough capacity: {}", System.currentTimeMillis() - startMethod );
     }
+
+    private boolean reasonableToCopyData( NodeWithAlloc node, Task task, NodeCache cache ) {
+        return true;
+    }
+
+    private static class NodeCache {
+
+        /**
+         * A <b>sorted</b> list of tasks that are currently running on the node.
+         */
+        private final List<Requirements> running;
+        /**
+         * A <b>sorted</b> list of tasks that are currently waiting on the node.
+         */
+        private final ArrayList<Requirements> waiting;
+
+        NodeCache( final Collection<Requirements> running, final List<Task> tasksAlreadyReady, final List<Task> tasksPlanedToCopy ) {
+
+            this.running = new LinkedList<>(running);
+            this.running.sort( requirementsComparator );
+
+            ArrayList<Requirements> waitingTemp = null;
+
+            // A list of tasks that are ready to run, or data is copied so that they can start soon.
+            if ( tasksAlreadyReady != null ) {
+                waitingTemp = tasksAlreadyReady.stream().map( Task::getRequest ).collect( Collectors.toCollection(ArrayList::new) );
+            }
+            if ( tasksPlanedToCopy != null && !tasksPlanedToCopy.isEmpty() ) {
+                final ArrayList<Requirements> planned = tasksPlanedToCopy.stream().map( Task::getRequest ).collect( Collectors.toCollection(ArrayList::new) );
+                if ( waitingTemp == null ) {
+                    waitingTemp = planned;
+                } else {
+                    waitingTemp.addAll( planned );
+                }
+            } else if ( waitingTemp == null ) {
+                waitingTemp = new ArrayList<>();
+            }
+
+            waiting = waitingTemp;
+            waiting.sort( requirementsComparator );
+
+        }
+
+        void addPlaned( Requirements requirement ) {
+            int insertionIndex = Collections.binarySearch( waiting, requirement, requirementsComparator );
+            if (insertionIndex < 0) {
+                insertionIndex = -(insertionIndex + 1);
+            }
+            waiting.add(insertionIndex, requirement);
+        }
+
+    }
+
+    private static class RequirementsComparator implements Comparator<Requirements> {
+
+        @Override
+        public int compare(Requirements r1, Requirements r2) {
+            int cpuComparison = r1.getCpu().compareTo(r2.getCpu());
+            if (cpuComparison != 0) {
+                return cpuComparison;
+            } else {
+                return r1.getRam().compareTo(r2.getRam());
+            }
+        }
+
+    }
+
 
 }
 
