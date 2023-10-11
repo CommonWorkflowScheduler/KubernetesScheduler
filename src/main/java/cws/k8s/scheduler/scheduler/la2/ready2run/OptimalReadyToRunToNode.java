731d401945544b77c4acee5b4f8051aa1943a57f
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2023-10-11 10:42:08 +0200

    OptimalReadyToRunToNode optimizer sops after 10 seconds
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/cws/k8s/scheduler/scheduler/la2/ready2run/OptimalReadyToRunToNode.java
+++ src/main/java/cws/k8s/scheduler/scheduler/la2/ready2run/OptimalReadyToRunToNode.java
@@ -2,7 +2,6 @@ package cws.k8s.scheduler.scheduler.la2.ready2run;
 
 import cws.k8s.scheduler.model.NodeWithAlloc;
 import cws.k8s.scheduler.model.Requirements;
-import cws.k8s.scheduler.model.location.hierachy.HierarchyWrapper;
 import cws.k8s.scheduler.scheduler.data.TaskInputsNodes;
 import cws.k8s.scheduler.util.LogCopyTask;
 import cws.k8s.scheduler.util.NodeTaskLocalFilesAlignment;
@@ -97,15 +96,16 @@ public class OptimalReadyToRunToNode implements ReadyToRunToNode {
             model.addLessOrEqual( cpuUsed.get( entry.getKey() ), availableByNode.get( entry.getKey() ).getCpu().multiply( MILLION ).longValue() );
         }
 
+        log.info( "Model created in " + (System.currentTimeMillis() - start) + "ms ( " + taskNodeBoolVars.size() + " vars )" );
         model.maximize( objective );
         CpSolver solver = new CpSolver();
+        solver.getParameters().setMaxTimeInSeconds( 10 );
         final CpSolverStatus solve = solver.solve( model );
-        final String message = "Solved in " + (System.currentTimeMillis() - start) + "ms ( " + taskNodeBoolVars.size() + " vars )";
+        final String message = "Solved in " + (System.currentTimeMillis() - start) + "ms ( " + taskNodeBoolVars.size() + " vars ) + solution is: " + solve;
         log.info( message );
         logger.log( message );
         log.info("Total packed value: " + solver.objectiveValue());
-        log.info( String.valueOf( solve ) );
-        if ( solve == CpSolverStatus.MODEL_INVALID ) {
+        if ( solve != CpSolverStatus.OPTIMAL && solve != CpSolverStatus.FEASIBLE ) {
             return Collections.emptyList();
         }
 
