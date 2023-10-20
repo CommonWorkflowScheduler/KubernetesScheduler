82aaf7f0faea83e0e90966456d5c18140ffdadb5
Florian Friederici <friederici@gmx.de>
2023-10-20 10:27:18 +0200

    round suggestions to ceiling

--- src/main/java/cws/k8s/scheduler/memory/SimpleOptimizer.java
+++ src/main/java/cws/k8s/scheduler/memory/SimpleOptimizer.java
@@ -18,6 +18,7 @@
 package cws.k8s.scheduler.memory;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,7 +28,14 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * SimpleOptimizer will use the following strategy:
- * tbd.
+ * 
+ * - In case task has failed, double memory
+ * - In case task was successful: reduce memory according to:
+ * 
+ *   if no old suggestion exists:
+ *    - let the new suggestion be 10% higher, then the peakRss was
+ *   else:
+ *    - new suggestion = (new peakRss + last suggestion) / 2
  * 
  * @author Florian Friederici
  *
@@ -45,24 +53,24 @@ class SimpleOptimizer implements MemoryOptimizer {
 	
 	@Override
 	public void addObservation(Observation o) {
-		log.debug("addObservation");
+		log.debug("SimpleOptimizer.addObservation");
 		this.observations.add(o);
 		
 		if (o.success) {
 			// decrease suggestion
 			if (suggestions.containsKey(o.task)) {
 				BigDecimal sug = ( o.peakRss.add(suggestions.get(o.task)) ).divide(new BigDecimal(2));
-				suggestions.replace(o.task, sug);
+				suggestions.replace(o.task, sug.setScale(0, RoundingMode.CEILING));
 			} else {
-				suggestions.put(o.task, o.peakRss.multiply(new BigDecimal(1.1)));
+				suggestions.put(o.task, o.peakRss.multiply(new BigDecimal(1.1)).setScale(0, RoundingMode.CEILING) );
 			}
 		} else {
 			// increase suggestion
 			if (suggestions.containsKey(o.task)) {
 				BigDecimal sug = suggestions.get(o.task).multiply(new BigDecimal(2));
-				suggestions.replace(o.task, sug);
+				suggestions.replace(o.task, sug.setScale(0, RoundingMode.CEILING));
 			} else {
-				suggestions.put(o.task, o.ramRequest.multiply(new BigDecimal(2)));
+				suggestions.put(o.task, o.ramRequest.multiply(new BigDecimal(2)).setScale(0, RoundingMode.CEILING) );
 			}
 		}
 		
