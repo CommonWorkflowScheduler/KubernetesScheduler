0c730f61ff6759bf843d7267d7f0ffdb29c604ad
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2024-03-07 17:31:33 +0100

    Fix tests
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/test/java/cws/k8s/scheduler/prediction/predictor/LinearPredictorTest.java
+++ src/test/java/cws/k8s/scheduler/prediction/predictor/LinearPredictorTest.java
@@ -11,32 +11,32 @@ public class LinearPredictorTest {
     @Test
     public void testOneTask() {
         LinearPredictor lp = getLinearPredictor();
-        lp.addTask( new TestTask( 1, 1 ) );
-        assertNull( lp.queryPrediction( new TestTask( 4, 4 ) ) );
+        lp.addTask( new TestTask( 1d, 1d ) );
+        assertNull( lp.queryPrediction( new TestTask( 4d, 4d ) ) );
     }
 
     @Test
     public void testTwoTasks() {
         LinearPredictor lp = getLinearPredictor();
-        lp.addTask( new TestTask( 1, 1 ) );
-        lp.addTask( new TestTask( 2, 2 ) );
-        assertEquals( (Double) 4d, lp.queryPrediction( new TestTask( 4, 4 ) ) );
+        lp.addTask( new TestTask( 1d, 1d ) );
+        lp.addTask( new TestTask( 2d, 2d ) );
+        assertEquals( (Double) 4d, lp.queryPrediction( new TestTask( 4d, 4d ) ) );
     }
 
     @Test
     public void testPredict() {
         LinearPredictor lp = getLinearPredictor();
-        lp.addTask( new TestTask( 1, 1 ) );
-        lp.addTask( new TestTask( 2, 2 ) );
-        lp.addTask( new TestTask( 3, 3 ) );
-        assertEquals( (Double) 4d, lp.queryPrediction( new TestTask( 4, 4 ) ) );
-        assertEquals(  (Double) 0d, lp.queryPrediction( new TestTask( 0, 0 ) ) );
+        lp.addTask( new TestTask( 1d, 1d ) );
+        lp.addTask( new TestTask( 2d, 2d ) );
+        lp.addTask( new TestTask( 3d, 3d ) );
+        assertEquals( (Double) 4d, lp.queryPrediction( new TestTask( 4d, 4d ) ) );
+        assertEquals(  (Double) 0d, lp.queryPrediction( new TestTask( 0d, 0d ) ) );
     }
 
     @Test
     public void noData() {
         LinearPredictor lp = getLinearPredictor();
-        assertNull(lp.queryPrediction( new TestTask( 4, 4 ) ));
+        assertNull(lp.queryPrediction( new TestTask( 4d, 4d ) ));
     }
 
     @NotNull
