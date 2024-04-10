d4f838280fc18e56216d971499e4721e1006fcc0
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2024-04-10 17:47:15 +0200

    added constructor
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/cws/k8s/scheduler/model/TaskMetrics.java
+++ src/main/java/cws/k8s/scheduler/model/TaskMetrics.java
@@ -1,13 +1,11 @@
 package cws.k8s.scheduler.model;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@RequiredArgsConstructor
 public class TaskMetrics {
 
     private final long ramRequest;
