140eff3291ba29d69571e683fa2f223fabd8fbc4
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2024-11-06 16:22:41 +0100

    Comment DataMissing.java
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/cws/k8s/scheduler/model/cluster/DataMissing.java
+++ src/main/java/cws/k8s/scheduler/model/cluster/DataMissing.java
@@ -6,6 +6,12 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
+/**
+ * This class is used to create a triple of a task, a node and a label count.
+ * It is used to keep track which tasks are not on a node.
+ */
 @Getter
 @EqualsAndHashCode
 @RequiredArgsConstructor
