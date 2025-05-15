d2f37f45decbba8efa1a1da0dbc0f3c24612defb
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2025-05-15 13:06:16 +0200

    Fix compilation problem
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/test/java/cws/k8s/scheduler/util/SortedListTest.java
+++ src/test/java/cws/k8s/scheduler/util/SortedListTest.java
@@ -73,7 +73,8 @@ class SortedListTest {
 
     @Test
     void addToEmptyList() {
-        final SortedList<Integer> sortedList = new SortedList<>( new LinkedList<>() );
+        // Explicit Integer required here
+        final SortedList<Integer> sortedList = new SortedList<Integer>( new LinkedList<Integer>() );
         sortedList.add( 4 );
         log.info( sortedList.toString() );
         assertEquals( 4, sortedList.get( 0 ) );
