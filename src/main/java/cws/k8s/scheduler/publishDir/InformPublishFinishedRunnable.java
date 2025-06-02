20e6983b95e9c20a09be8b7d96926cc148e04d65
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2025-06-02 15:38:26 +0200

    Limit number of parallel publish operations per node
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- src/main/java/cws/k8s/scheduler/publishDir/InformPublishFinishedRunnable.java
+++ src/main/java/cws/k8s/scheduler/publishDir/InformPublishFinishedRunnable.java
@@ -3,15 +3,19 @@ package cws.k8s.scheduler.publishDir;
 import cws.k8s.scheduler.model.location.NodeLocation;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 public class InformPublishFinishedRunnable implements Runnable {
 
     private final PublishExecHolder execHolder;
     private final NodeLocation nodeLocation;
+    private final Map<NodeLocation, Integer> currentPublishJobsPerNode;
 
 
     @Override
     public void run() {
         execHolder.finishedOnNode( nodeLocation );
+        currentPublishJobsPerNode.compute( nodeLocation, ( k, v ) -> v == null ? 0 : v - 1 );
     }
 }
